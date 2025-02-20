e2627bcaee173bc1f6ec8faa61710a78d333cc5d
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-02-20 20:04:22 +0530

    add frontend

--- STRINGS/danceui/src/App.js
+++ STRINGS/danceui/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Compare from "./component/Compare";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Compare />
+		</div>
+	);
 }
 
 export default App;
