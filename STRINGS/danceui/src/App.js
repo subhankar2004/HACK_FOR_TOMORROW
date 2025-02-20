d859ed59fd4a4a225230949614ac736330c2b3f8
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-02-20 23:16:39 +0530

    create component

--- STRINGS/danceui/src/App.js
+++ STRINGS/danceui/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Compare from "./component/Compare";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
 
 function App() {
 	return (
 		<div className='App'>
+			<Navbar />
+			{/* <Home /> */}
 			<Compare />
 		</div>
 	);
---
+++ STRINGS/danceui/src/component/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<header className='navbar'>
+			<div className='logo'>
+				<a href='/'>Dance Zero</a>
+			</div>
+			<nav className={`nav-links ${isOpen ? "active" : ""}`}>
+				<a href='/'>Home</a>
+				<a href='/about'>Dance-Compare</a>
+				<a href='/services'>Feedback</a>
+				<a
+					href='/login'
+					className='login-btn'>
+					<i className='fa fa-user'></i> Logout
+				</a>
+				<button className='search-btn'>
+					<i className='fa fa-search'></i>
+				</button>
+			</nav>
+			<div
+				className='menu-toggle'
+				onClick={toggleMenu}>
+				☰
+			</div>
+		</header>
+	);
+};
+
+export default Navbar;
