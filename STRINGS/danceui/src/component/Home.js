45183bcf08842bb2a94f10b853692e3fbe3fe31b
Adyasha sahoo <workadyasha@gmail.com>
2025-02-21 12:57:21 +0530

    and image

--- STRINGS/danceui/src/component/Home.js
+++ STRINGS/danceui/src/component/Home.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Home.css";
+import danceImage from "../assets/UI3-removebg-preview.png"; // Importing the image
 
 const Home = () => {
-	return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <div className="text-container">
+        <p className="title">Groove wherever you want!!</p>
+        <h1 className="subtitle">
+          Get your moves so accurate that anyone can't even compare to you! HAHAHA
+        </h1>
+        <button className="read-more-btn">Read more about this app</button>
+      </div>
+
+      <div className="image-container">
+        <img src={danceImage} alt="Dancing bro" className="dance-image" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
